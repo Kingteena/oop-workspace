178e4aa5b5be32eaf56839f35b13830c31fc0ff8
Kingteena <a1930250@student.adelaide.edu.au>
2024-08-16 13:31:48 +0930

    fix bug with extra dependencies

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -4,7 +4,21 @@
 
 using namespace std;
 
-extern PersonList createPersonList(int);
+PersonList createPersonList(int n){
+    PersonList person_list;
+
+    person_list.numPeople = n;
+    person_list.people = new Person[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        person_list.people[i].name = "Jane Doe";
+        person_list.people[i].age = 1;
+    }
+
+    return person_list;
+}
+
 extern PersonList deepCopyPersonList(PersonList);
 
 int main() {
