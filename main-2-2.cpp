17f82c4273163129c6914e45401098fdc682decd
Kingteena <a1930250@student.adelaide.edu.au>
2024-09-06 16:22:10 +0930

    nvm this one final

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -14,7 +14,7 @@ int main() {
   myAirVehicle.set_fuel(95);
   myAirVehicle.set_numberOfFlights(100);
   myAirVehicle.set_weight(5690);
-  myAirVehicle.reducePassenders(100);
+  myAirVehicle.reducePassengers(100);
   myAirVehicle.fly(65, 120);
 
   cout << "fuel: " << myAirVehicle.get_fuel() << endl;
@@ -22,7 +22,7 @@ int main() {
   cout << "numberOfFlights: " << myAirVehicle.get_numberOfFlights() << endl;
   cout << "numPassengers: " << myAirVehicle.get_numPassengers() << endl;
 
-  myAirVehicle.reducePassenders(1000);
+  myAirVehicle.reducePassengers(1000);
   cout << "numPassengers: " << myAirVehicle.get_numPassengers() << endl;
 
   return 0;
