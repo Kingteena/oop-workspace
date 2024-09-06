ec4d1ca2d79d068ba7ecb6bee28648e5e82fc2ec
Kingteena <a1930250@student.adelaide.edu.au>
2024-09-06 16:21:04 +0930

    final commit pls

--- AirFleet.cpp
+++ AirFleet.cpp
@@ -6,15 +6,30 @@
 
 AirFleet::AirFleet() {
   fleet = new AirVehicle*[5];
-  *fleet[0] = Airplane(20, 10);
-  *fleet[1] = Helicopter(1000, "BlackHawk");
-  *fleet[2] = AirVehicle(5000);
-  *fleet[3] = Helicopter(100, "WhiteHawk");
-  *fleet[4] = Airplane(15, 20);
+  AirVehicle *temp0 = new AirVehicle, *temp1 = new AirVehicle,
+             *temp2 = new AirVehicle, *temp3 = new AirVehicle,
+             *temp4 = new AirVehicle;
+
+  *temp0 = Airplane(20, 10);
+  fleet[0] = temp0;
+
+  *temp1 = Helicopter(1000, "BlackHawk");
+  fleet[1] = temp1;
+
+  *temp2 = AirVehicle(5000);
+  fleet[2] = temp2;
+
+  *temp3 = Helicopter(100, "WhiteHawk");
+  fleet[3] = temp3;
+
+  *temp4 = Airplane(15, 20);
+  fleet[4] = temp4;
 }
 
-AirFleet::~AirFleet() {
-  delete[] fleet;
+AirFleet::~AirFleet() { for (int i = 0; i < 5; i++)
+{
+    delete fleet[i];
 }
+delete[] fleet; }
 
 AirVehicle** AirFleet::get_fleet() { return fleet; }
