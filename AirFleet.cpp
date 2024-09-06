f1bc532a7252634d6e47d1935f046150c458b8ee
Kingteena <a1930250@student.adelaide.edu.au>
2024-09-06 16:31:21 +0930

    static?

--- AirFleet.cpp
+++ AirFleet.cpp
@@ -6,24 +6,28 @@
 
 AirFleet::AirFleet() {
   fleet = new AirVehicle*[5];
-  AirVehicle *temp0 = new AirVehicle, *temp1 = new AirVehicle,
-             *temp2 = new AirVehicle, *temp3 = new AirVehicle,
-             *temp4 = new AirVehicle;
+  //   AirVehicle *temp0 = new AirVehicle, *temp1 = new AirVehicle,
+  //  *temp2 = new AirVehicle, *temp3 = new AirVehicle,
+  //  *temp4 = new AirVehicle;
 
-  *temp0 = Airplane(20, 10);
-  fleet[0] = temp0;
+  Airplane temp0, temp4;
+  Helicopter temp1, temp3;
+  AirVehicle temp2;
 
-  *temp1 = Helicopter(1000, "BlackHawk");
-  fleet[1] = temp1;
+  temp0 = Airplane(20, 10);
+  fleet[0] = &temp0;
 
-  *temp2 = AirVehicle(5000);
-  fleet[2] = temp2;
+  temp1 = Helicopter(1000, "BlackHawk");
+  fleet[1] = &temp1;
 
-  *temp3 = Helicopter(100, "WhiteHawk");
-  fleet[3] = temp3;
+  temp2 = AirVehicle(5000);
+  fleet[2] = &temp2;
 
-  *temp4 = Airplane(15, 20);
-  fleet[4] = temp4;
+  temp3 = Helicopter(100, "WhiteHawk");
+  fleet[3] = &temp3;
+
+  temp4 = Airplane(15, 20);
+  fleet[4] = &temp4;
 }
 
 AirFleet::~AirFleet() { delete[] fleet; }
