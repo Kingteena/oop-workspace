d63555fd603d75f8059290889e7266cfc95e77cf
Kingteena <a1930250@student.adelaide.edu.au>
2024-09-06 16:42:22 +0930

    ahhhhHHHHHH

--- AirFleet.cpp
+++ AirFleet.cpp
@@ -6,13 +6,6 @@
 
 AirFleet::AirFleet() {
   fleet = new AirVehicle*[5];
-  //   AirVehicle *temp0 = new AirVehicle, *temp1 = new AirVehicle,
-  //  *temp2 = new AirVehicle, *temp3 = new AirVehicle,
-  //  *temp4 = new AirVehicle;
-
-//   Airplane temp0, temp4;
-//   Helicopter temp1, temp3;
-//   AirVehicle temp2;
 
   Airplane *temp0 = new Airplane , *temp4 = new Airplane;
   Helicopter *temp1 = new Helicopter, *temp3 = new Helicopter;
@@ -21,7 +14,7 @@ AirFleet::AirFleet() {
   *temp0 = Airplane(20, 10);
   fleet[0] = temp0;
 
-  *temp1 = Helicopter(1000, "BlackHawk");
+  *temp1 = Helicopter(10000, "BlackHawk");
   fleet[1] = temp1;
 
   *temp2 = AirVehicle(5000);
